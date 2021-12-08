9cf92a80fdb823699bf639526a5135e9953ed14c
Anatoliy Komarov <admin@komaroff.biz>
2021-12-08 11:51:03 +0300

    2

--- wp-content/themes/black/functions/functions_theme.php
+++ wp-content/themes/black/functions/functions_theme.php
@@ -10,9 +10,9 @@ function showSocialIcons(){
     global $instagram_link;
     global $facebook_link;
     global $post_mail;
-    echo '<a href="'.$linkedin_link.'"><img src="'.get_template_directory_uri().'/images/linkedin.svg" alt=linkedin link""></a>
-                <a href="'.$instagram_link.'"><img src="'.get_template_directory_uri().'/images/facebook.svg" alt="facebook link"></a>
-                <a href="'.$facebook_link.'"><img src="'.get_template_directory_uri().'/images/instagram.svg" alt=" instagram link"></a>';
+    echo '<a target="_blank" href="'.$linkedin_link.'"><img src="'.get_template_directory_uri().'/images/linkedin.svg" alt="yotube link"></a>
+                <a target="_blank" href="'.$facebook_link.'"><img src="'.get_template_directory_uri().'/images/facebook.svg" alt="facebook link"></a>
+                <a target="_blank" href="'.$instagram_link.'"><img src="'.get_template_directory_uri().'/images/instagram.svg" alt=" instagram link"></a>';
 }
 
 add_shortcode('socials','showSocialIcons');
